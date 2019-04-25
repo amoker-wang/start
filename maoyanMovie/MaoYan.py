f19f41a82820a6d90fcce7cbc3a9b6beb1146fbc
amoker-pc <amoker.wang@gmail.com>
2019-04-26 01:04:48 +0800

    delete extra lib

--- maoyanMovie/MaoYan.py
+++ maoyanMovie/MaoYan.py
@@ -1,9 +1,8 @@
-import json
 import requests
 from requests.exceptions import RequestException
 import re
 import time
-
+import pymongo
 
 def get_one_page(url):
     try:
@@ -32,20 +31,18 @@ def parse_one_page(html):
             'time': item[4].strip()[5:],
             'score': item[5] + item[6]
         }
-
-
 def write_to_file(content):
-    with open('result.txt', 'a', encoding='utf-8') as f:
-        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+    client = pymongo.MongoClient('mongodb://localhost:27017/local')
+    db = client.local
+    collection = db.maoyan
+    collection.insert(content)
 
 
 def main(offset):
     url = 'http://maoyan.com/board/4?offset=' + str(offset)
     html = get_one_page(url)
     for item in parse_one_page(html):
-        print(item)
         write_to_file(item)
-
 
 if __name__ == '__main__':
     for i in range(10):
---
+++ toutiao/jiepai.py
@@ -0,0 +1,95 @@
+import requests
+from urllib.parse import urlencode
+from requests import codes
+import os
+from hashlib import md5
+from multiprocessing.pool import Pool
+import re
+
+
+def get_page(offset):
+    params = {
+        'aid': '24',
+        'offset': offset,
+        'format': 'json',
+        # 'keyword': 'python',
+        'autoload': 'true',
+        'count': '20',
+        'cur_tab': '1',
+        'from': 'search_tab',
+        'pd': 'synthesis'
+    }
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
+    }
+    base_url = 'https://www.toutiao.com/api/search/content?keyword=xiaomi'
+    url = base_url + urlencode(params)
+    try:
+        resp = requests.get(url,headers=headers)
+        print(url)
+        if 200 == resp.status_code:
+            print(resp.json())
+            return resp.json()
+    except requests.ConnectionError:
+        return None
+
+
+def get_images(json):
+    if json.get('data'):
+        data = json.get('data')
+        for item in data:
+            if item.get('cell_type') is not None:
+                continue
+            title = item.get('title')
+            images = item.get('image_list')
+            for image in images:
+                origin_image = re.sub("list", "origin", image.get('url'))
+                yield {
+                    'image': origin_image,
+                    # 'iamge': image.get('url'),
+                    'title': title
+                }
+
+
+print('succ')
+
+
+def save_image(item):
+    img_path = 'img' + os.path.sep + item.get('title')
+    print('succ2')
+    if not os.path.exists(img_path):
+        os.makedirs(img_path)
+    try:
+        resp = requests.get(item.get('image'))
+        if codes.ok == resp.status_code:
+            file_path = img_path + os.path.sep + '{file_name}.{file_suffix}'.format(
+                file_name=md5(resp.content).hexdigest(),
+                file_suffix='jpg')
+            if not os.path.exists(file_path):
+                print('succ3')
+                with open(file_path, 'wb') as f:
+                    f.write(resp.content)
+                print('Downloaded image path is %s' % file_path)
+                print('succ4')
+            else:
+                print('Already Downloaded', file_path)
+    except requests.ConnectionError:
+        print('Failed to Save Image，item %s' % item)
+
+
+def main(offset):
+    json = get_page(offset)
+    for item in get_images(json):
+        print(item)
+        save_image(item)
+
+
+GROUP_START = 0
+GROUP_END = 7
+
+if __name__ == '__main__':
+    pool = Pool()
+    groups = ([x * 20 for x in range(GROUP_START, GROUP_END + 1)])
+    pool.map(main, groups)
+    pool.close()
+    pool.join()
